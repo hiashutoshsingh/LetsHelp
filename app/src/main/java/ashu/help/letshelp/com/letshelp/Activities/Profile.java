2ef14375b0a18e4b08be0c4667e91ae6a841573c
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-01 19:39:07 +0530

    profile code added

--- app/src/main/java/ashu/help/letshelp/com/letshelp/Activities/Profile.java
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Activities/Profile.java
@@ -2,14 +2,22 @@ package ashu.help.letshelp.com.letshelp.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import ashu.help.letshelp.com.letshelp.R;
 
 public class Profile extends AppCompatActivity {
 
+    private TextView name,gender,emailid,phoneno;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        name=(TextView)findViewById(R.id.profile_name);
+        gender=(TextView)findViewById(R.id.profile_gender);
+        emailid=(TextView)findViewById(R.id.profile_email);
+        phoneno=(TextView)findViewById(R.id.profile_Number);
     }
 }
