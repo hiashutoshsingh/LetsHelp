f79ff71217c6c637492cf92f944fb93cebff1f9e
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-02 03:06:10 +0530

    share intent added

--- app/src/main/java/ashu/help/letshelp/com/letshelp/Activities/Home.java
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Activities/Home.java
@@ -104,6 +104,12 @@ public class Home extends AppCompatActivity
         }
         else if (id == R.id.nav_send) {
 
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            String shareBody = "An app to help out the people by donating money, clothes and blood";
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "LetsHelp \n");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+            startActivity(Intent.createChooser(sharingIntent, "Share via"));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
