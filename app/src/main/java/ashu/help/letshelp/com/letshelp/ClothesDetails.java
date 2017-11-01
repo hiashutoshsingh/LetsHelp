38e91ed406944cb93b0d3ca30a844eb840588687
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-02 03:14:07 +0530

    cloth details code added

--- app/src/main/java/ashu/help/letshelp/com/letshelp/ClothesDetails.java
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/ClothesDetails.java
@@ -1,10 +1,14 @@
 package ashu.help.letshelp.com.letshelp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 public class ClothesDetails extends AppCompatActivity {
 
@@ -17,6 +21,31 @@ public class ClothesDetails extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final String money_details = getIntent().getExtras().getString("clothdetails");
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_cloth);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "www.playstore.com/letshelp/appid=123sde45  \n"+ money_details);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+
+
+        clothdetails=(TextView)findViewById(R.id.clothdetails);
+        clothdetails.setText(money_details);
+
+        button=(Button)findViewById(R.id.clothregister);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ClothesDetails.this, RegisterClothesDonation.class));
+                finish();
+            }
+        });
     }
 }
