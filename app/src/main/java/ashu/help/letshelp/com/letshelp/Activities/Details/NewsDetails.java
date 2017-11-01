bc5dcaed17b9673b2bde38d784f62417e7f4e655
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-02 03:35:39 +0530

    newsdetails layout

---
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Activities/Details/NewsDetails.java
@@ -0,0 +1,25 @@
+package ashu.help.letshelp.com.letshelp.Activities.Details;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import ashu.help.letshelp.com.letshelp.R;
+public class NewsDetails extends AppCompatActivity {
+
+    TextView newsdetails;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+       
+    }
+}
