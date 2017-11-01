3e7b67aff6b8dc783ac034077d6d65051f8bce82
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-02 00:21:16 +0530

    doante money code added

--- app/src/main/java/ashu/help/letshelp/com/letshelp/Fragments/DonateMoney.java
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Fragments/DonateMoney.java
@@ -3,12 +3,15 @@ package ashu.help.letshelp.com.letshelp.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ashu.help.letshelp.com.letshelp.R;
+import letshelp.letshelpngo.com.letshelp.MoneyAdapter.MoneyAdapter;
+import letshelp.letshelpngo.com.letshelp.R;
 
 
 public class DonateMoney extends Fragment {
@@ -24,7 +27,11 @@ public class DonateMoney extends Fragment {
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_donate_money,container,false);
 
-       
+        recyclerView=(RecyclerView)view.findViewById(R.id.recyclerView_donate_money);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        MoneyAdapter moneyAdapter=new MoneyAdapter();
+        recyclerView.setAdapter(moneyAdapter);
 
         return view;
     }
