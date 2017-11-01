04c7228a2f05d794dea9a96c1e5e390176811850
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-02 02:36:27 +0530

    blooddonate activity layout added

---
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Activities/BloodDonateActivity.java
@@ -0,0 +1,64 @@
+package ashu.help.letshelp.com.letshelp.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import ashu.help.letshelp.com.letshelp.R;
+import letshelp.letshelpngo.com.letshelp.Fragments.BloodDonate;
+import letshelp.letshelpngo.com.letshelp.Fragments.DonorsMap;
+import letshelp.letshelpngo.com.letshelp.R;
+
+public class BloodDonateActivity extends AppCompatActivity {
+
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_blood_donate);
+        setContentView(R.layout.activity_blood_donate);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_blood_donate, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
