089b600c5d144358339460da6639f99f0fc722c4
kishluv782 <atharvkrn782@gmail.com>
2017-11-01 20:13:13 +0530

    AdminPanel Layout add

---
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Admin/AdminPanel.java
@@ -0,0 +1,18 @@
+package ashu.help.letshelp.com.letshelp.Admin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import ashu.help.letshelp.com.letshelp.R;
+
+public class AdminPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_panel_layout);
+    }
+
+}
