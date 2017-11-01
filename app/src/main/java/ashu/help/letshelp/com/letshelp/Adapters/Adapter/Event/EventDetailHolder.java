47c713e3c8a04b3047c4673f569be2ee8c679f16
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-02 03:38:36 +0530

    listner added for events

--- app/src/main/java/ashu/help/letshelp/com/letshelp/Adapters/Adapter/Event/EventDetailHolder.java
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Adapters/Adapter/Event/EventDetailHolder.java
@@ -1,11 +1,13 @@
 package ashu.help.letshelp.com.letshelp.Adapters.Adapter.Event;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import ashu.help.letshelp.com.letshelp.Activities.Details.EventsDetails;
 import ashu.help.letshelp.com.letshelp.R;
 
 
@@ -21,7 +23,56 @@ class EventDetailHolder extends RecyclerView.ViewHolder {
 
         skillImage=(ImageView)itemView.findViewById(R.id.imageView_article);
         skillName=(TextView) itemView.findViewById(R.id.textView_aboutArticle);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getAdapterPosition()==0)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, EventsDetails.class);
+                    newActivity.putExtra("eventdetails", s);
+                    context.startActivity(newActivity);
 
+                }
+                if (getAdapterPosition()==1)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, EventsDetails.class);
+                    newActivity.putExtra("eventdetails", s);
+                    context.startActivity(newActivity);
+                }
+                if (getAdapterPosition()==2)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, EventsDetails.class);
+                    newActivity.putExtra("eventdetails", s);
+                    context.startActivity(newActivity);
+
+                }if (getAdapterPosition()==3)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, EventsDetails.class);
+                    newActivity.putExtra("eventdetails", s);
+                    context.startActivity(newActivity);
+                }if (getAdapterPosition()==4)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, EventsDetails.class);
+                    newActivity.putExtra("eventdetails", s);
+                    context.startActivity(newActivity);
+                }
+                if (getAdapterPosition()==5)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, EventsDetails.class);
+                    newActivity.putExtra("eventdetails", s);
+                    context.startActivity(newActivity);
+                }
+
+
+
+            }
+        });
     }
 
     public void bindData(EventHolder eventHolder)
