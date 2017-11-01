99173c9ad843efbe457cb9536c065cb166afccec
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-02 03:31:12 +0530

    moneydetails code

--- app/src/main/java/ashu/help/letshelp/com/letshelp/Activities/Details/MoneyDetails.java
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Activities/Details/MoneyDetails.java
@@ -8,9 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import ashu.help.letshelp.com.letshelp.R;
 
-import letshelp.letshelpngo.com.letshelp.Activity.Payment;
-import letshelp.letshelpngo.com.letshelp.R;
 
 public class MoneyDetails extends AppCompatActivity {
 
@@ -24,6 +23,35 @@ public class MoneyDetails extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-      
+        final String money_details = getIntent().getExtras().getString("moneydetails");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_money);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "www.playstore.com/letshelp/appid=123sde45  \n"+ money_details);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+
+        continue_payment=(Button)findViewById(R.id.continue_payment);
+        continue_payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//            startActivity(new Intent(MoneyDetails.this, Payment.class));
+
+            }
+        });
+
+
+
+        moneydetails=(TextView)findViewById(R.id.money_details);
+        moneydetails.setText(money_details);
+
+
     }
 }
