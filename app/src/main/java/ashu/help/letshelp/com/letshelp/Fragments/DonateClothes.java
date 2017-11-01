c6cced1c3df4389fee8f6755748e9229ca57e3ad
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-02 00:46:53 +0530

    donate clothes code added

--- app/src/main/java/ashu/help/letshelp/com/letshelp/Fragments/DonateClothes.java
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Fragments/DonateClothes.java
@@ -2,12 +2,15 @@ package ashu.help.letshelp.com.letshelp.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ashu.help.letshelp.com.letshelp.R;
+import letshelp.letshelpngo.com.letshelp.ClothesAdapter.ClothesAdapter;
+import letshelp.letshelpngo.com.letshelp.R;
 
 
 public class DonateClothes extends Fragment {
@@ -23,9 +26,14 @@ public class DonateClothes extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         View view=inflater.inflate(R.layout.fragment_donate_clothes,container,false);
 
-
+        recyclerView=(RecyclerView)view.findViewById(R.id.recyclerView_donate_clothes);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        ClothesAdapter clothesAdapter=new ClothesAdapter();
+        recyclerView.setAdapter(clothesAdapter);
 
         return view;
     }
