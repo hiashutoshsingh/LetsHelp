2e6cf492b2ad88cdfd0b490ff4487e2d05424478
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-02 12:11:07 +0530

    listners of news

--- app/src/main/java/ashu/help/letshelp/com/letshelp/Adapters/Adapter/NewsAdapter/NewsDetailHolder.java
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Adapters/Adapter/NewsAdapter/NewsDetailHolder.java
@@ -1,11 +1,13 @@
 package ashu.help.letshelp.com.letshelp.Adapters.Adapter.NewsAdapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import ashu.help.letshelp.com.letshelp.Activities.Details.NewsDetails;
 import ashu.help.letshelp.com.letshelp.R;
 
 
@@ -22,7 +24,55 @@ class NewsDetailHolder extends RecyclerView.ViewHolder {
         skillImage=(ImageView)itemView.findViewById(R.id.imageView_article);
         skillName=(TextView) itemView.findViewById(R.id.textView_aboutArticle);
 
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getAdapterPosition()==0)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, NewsDetails.class);
+                    newActivity.putExtra("newsdetails", s);
+                    context.startActivity(newActivity);
 
+                }
+                if (getAdapterPosition()==1)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, NewsDetails.class);
+                    newActivity.putExtra("newsdetails", s);
+                    context.startActivity(newActivity);
+                }
+                if (getAdapterPosition()==2)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, NewsDetails.class);
+                    newActivity.putExtra("newsdetails", s);
+                    context.startActivity(newActivity);
+                }if (getAdapterPosition()==3)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, NewsDetails.class);
+                    newActivity.putExtra("newsdetails", s);
+                    context.startActivity(newActivity);
+                }if (getAdapterPosition()==4)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, NewsDetails.class);
+                    newActivity.putExtra("newsdetails", s);
+                    context.startActivity(newActivity);
+                }
+                if (getAdapterPosition()==5)
+                {
+                    String s=String.valueOf(skillName.getText());
+                    Intent newActivity = new Intent(context, NewsDetails.class);
+                    newActivity.putExtra("newsdetails", s);
+                    context.startActivity(newActivity);
+                }
+
+
+
+            }
+        });
     }
 
     public void bindData(NewsHolder newsHolder)
