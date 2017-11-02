a085bdc5af3c68db9934cb32b314ea92fdc1bb31
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-02 15:27:45 +0530

    final hackanthon commit

--- app/src/main/java/ashu/help/letshelp/com/letshelp/Activities/Splash.java
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Activities/Splash.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import ashu.help.letshelp.com.letshelp.Fragments.Choose_Act;
+
 
 public class Splash extends AppCompatActivity {
     @Override
@@ -15,7 +17,9 @@ public class Splash extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        startActivity(new Intent(this,AuthActivity.class));
+        startActivity(new Intent(this,Choose_Act.class));
         finish();
+
+
     }
 }
