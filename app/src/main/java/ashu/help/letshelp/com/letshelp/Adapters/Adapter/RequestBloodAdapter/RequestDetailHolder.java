d4ee5f6f2208c301e13cc1592342824076776092
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2017-11-10 09:25:25 +0530

    map integated

--- app/src/main/java/ashu/help/letshelp/com/letshelp/Adapters/Adapter/RequestBloodAdapter/RequestDetailHolder.java
+++ app/src/main/java/ashu/help/letshelp/com/letshelp/Adapters/Adapter/RequestBloodAdapter/RequestDetailHolder.java
@@ -1,11 +1,18 @@
 package ashu.help.letshelp.com.letshelp.Adapters.Adapter.RequestBloodAdapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import ashu.help.letshelp.com.letshelp.Activities.MapsActivity;
 import ashu.help.letshelp.com.letshelp.R;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 
 class RequestDetailHolder extends RecyclerView.ViewHolder {
@@ -13,6 +20,7 @@ class RequestDetailHolder extends RecyclerView.ViewHolder {
     private TextView name;
     private TextView bloodgroup;
     private TextView units;
+    private ImageView share,direction,phone;
 
     Context context;
 
@@ -24,6 +32,42 @@ class RequestDetailHolder extends RecyclerView.ViewHolder {
         name=(TextView)itemView.findViewById(R.id.receiver_name);
         bloodgroup=(TextView)itemView.findViewById(R.id.receiver_blood_group);
         units=(TextView)itemView.findViewById(R.id.receiver_blood_units);
+        share=(ImageView)itemView.findViewById(R.id.request_share);
+        direction=(ImageView)itemView.findViewById(R.id.request_direction);
+        phone=(ImageView)itemView.findViewById(R.id.request_phone);
+
+share.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = "An app to help out the people by donating money, clothes and blood";
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "LetsHelp \n");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+      context.startActivity(Intent.createChooser(sharingIntent, "Share via"));
+    }
+});
+
+        direction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myactivity = new Intent(context.getApplicationContext(), MapsActivity.class);
+                myactivity.addFlags(FLAG_ACTIVITY_NEW_TASK);
+                context.getApplicationContext().startActivity(myactivity);
+            }
+        });
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:9457444841"));
+                try {
+                   context.startActivity(in);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(context, "Could not find an activity to place the call.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
 
